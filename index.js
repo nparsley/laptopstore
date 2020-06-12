ea290845f57da9b4a48a367ac37d93b9fddb4f24
nickparsley <nickparsley5@gmail.com>
2020-06-12 07:40:48 +0000

    card & overview html

--- index.js
+++ index.js
@@ -10,13 +10,27 @@ const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
 
-
+    //PRODUCTS OVERVIEW
     if (pathName === '/products' || pathName === '/') {
         res.writeHead(200, { 'Content-type': 'text/html'});
-        res.end('This is the PRODUCTS page');
+        
+        fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+            
+            res.end(data);
+        });
+
+
+    //LAPTOP DETAIL
     } else if (pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, { 'Content-type': 'text/html'});
-        res.end(`This is the LAPTOP page for laptop ${id}!`);
+        
+        fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+            const laptop = laptopData[id];
+            const output = replaceTemplate(data, laptop);
+            res.end(output);
+        });
+
+    //URL NOT FOUND
     } else {
         res.writeHead(404, { 'Content-type': 'text/html'});
         res.end('URL was not found on the server');       
@@ -28,3 +42,16 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now');
 });
+
+function replaceTemplate(originalHTML, laptop) {
+        let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+        output = output.replace(/{%IMAGE%}/g, laptop.image);
+        output = output.replace(/{%PRICE%}/g, laptop.price);
+        output = output.replace(/{%SCREEN%}/g, laptop.screen);
+        output = output.replace(/{%CPU%}/g, laptop.cpu);
+        output = output.replace(/{%STORAGE%}/g, laptop.storage);
+        output = output.replace(/{%RAM%}/g, laptop.ram);
+        output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+        output = output.replace(/{%ID%}/g, laptop.id);
+        return output;
+}
